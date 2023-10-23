f25c7433741f2990c8156c0d0308902b3444d15d
ahita1 <ahitafani833@gmail.com>
2023-10-23 16:35:55 +0300

    Add New Feature

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("button clicked haha!!!")
+      }></button>
+    </div>
+  )
+}
+
+export default ProductCard

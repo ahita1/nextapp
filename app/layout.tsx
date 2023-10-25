362099cf14808f11f665f5bb3c787292953c5b91
ahita1 <ahitafani833@gmail.com>
2023-10-25 22:07:27 +0300

    Add New feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="emerald">
+      <body className={inter.className}>
+        <NavBar />
+        <main className='p-5'>{children}</main>
+      </body>
     </html>
-  )
+  );
 }

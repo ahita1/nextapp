c852d522705d3d79886f0b4ee8939774f477fc15
ahita1 <ahitafani833@gmail.com>
2023-10-27 18:31:14 +0300

    Getting a single Object haha

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,12 @@
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export function GET(
+    request: NextRequest,
+    { params }: { params: { id: number } }) {
+    if (params.id > 10)
+        return NextResponse.json({ error: 'User not Found!' }, { status: 404 })
+    return NextResponse.json({id : 16912 , name : 'Ahita'})
+    
+}

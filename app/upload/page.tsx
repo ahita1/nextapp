f2b70429e5f7da1cb0b50f1dbedb3e745c17d53c
ahita1 <ahitafani833@gmail.com>
2023-11-07 12:38:26 +0300

    some additional modifications haha

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -4,10 +4,10 @@ import { CldUploadWidget } from "next-cloudinary";
 
 const UploadPage = () => {
   return (
-    <CldUploadWidget uploadPreset="ar5zxkvm">
+    <CldUploadWidget onUpload={(result , widget) => {console.log(result);
+    }} uploadPreset="ar5zxkvm">
       {({ open }) => <button onClick  = {() => open()} className="btn btn-primary"> Upload</button>}
     </CldUploadWidget>
   );
 };
-
 export default UploadPage;

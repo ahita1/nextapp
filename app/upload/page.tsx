fc480d965d0044cc1fda708b89d33d43ac463ad8
ahita1 <ahitafani833@gmail.com>
2023-11-07 13:02:37 +0300

    showing the uploaded images haha

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,13 +1,32 @@
 'use client'
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface Cloudinary {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget onUpload={(result , widget) => {console.log(result);
-    }} uploadPreset="ar5zxkvm">
-      {({ open }) => <button onClick  = {() => open()} className="btn btn-primary"> Upload</button>}
-    </CldUploadWidget>
+    <>
+      { publicId && <CldImage src={ publicId } height={270} width={180} alt="Ahita's short photo haha" />}
+      <CldUploadWidget
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as Cloudinary;
+          setPublicId(info.public_id);
+        }}
+        uploadPreset="ar5zxkvm"
+      >
+        {({ open }) => (
+          <button onClick={() => open()} className="btn btn-primary">
+            {" "}
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 export default UploadPage;

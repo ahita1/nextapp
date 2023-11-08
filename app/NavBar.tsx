ccee7eaff3bae1d4ebd0d966d325f34c4a37364e
ahita1 <ahitafani833@gmail.com>
2023-11-08 23:56:30 +0300

    Add new Features

---
+++ app/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5 justify-between">
+      <div>
+        <Link className="mr-5" href="/">
+          EthioGebeya
+        </Link>
+        <Link href="/users">Users</Link>
+      </div>
+      <Link href="/api/auth/signin">Sign In</Link>
+    </div>
+  );
+}
+
+export default NavBar

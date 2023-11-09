f49a66408b5c4acfd911d70a01de5517697cf89f
ahita1 <ahitafani833@gmail.com>
2023-11-09 10:59:31 +0300

    Signing out a user haha

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar = () => {
         </Link>
         <Link href="/users">Users</Link>
       </div>
-      {status === 'authenticated' && <div>{ session.user!.name}</div>}
+      {status === 'authenticated' && <div>{ session.user!.name} <Link className="ml-3" href='/api/auth/signout'>Sign Out</Link></div>}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Sign In</Link>
       )}

4a17ec3196843c5becedbf739283cf761a18d31a
ahita1 <ahitafani833@gmail.com>
2023-11-12 18:33:00 +0300

    Ending email haha

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -4,13 +4,15 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST() {
-    await resend.emails.send({
-        from: 'ahitafani833@gmail.com',
-        to: "amsalusolomon12@gmail.com",
-        subject: '...',
+export async function GET() {
+        await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: "nigussieahunem@gmail.com",
+        subject: 'My name is Ahunem Nigussie, Im a fullstack developer but specializing on Front-end website developments with React and Vue.js frameworks haha',
         react : <WelcomeTemplate name="Ahita haha"/>
     })
 
-    return NextResponse
+    return NextResponse.json({
+        status : "ok"
+    })
 }
--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -12,19 +12,20 @@ import {
 const WelcomeTemplate = ({ name } : { name : string }) => {
   return (
     <Html>
-      <Preview>Welcome haha</Preview>
+      <Preview>Hey Soll Welcome haha</Preview>
       <Tailwind>
         <Body className="bg-white">
           <Container>
-                    <Text className="text-3xl font-bold">Hey {name} haha</Text>
-                    <Link href="https://ahitafani833.com">www.ahitafani833.com</Link>
+            <Text className="text-3xl font-bold">Hey Soll haha</Text>
+            <Text>I have made the website that you can easily access diffrent games that works on diffrent platforms</Text>
+            <Text className="text-purple-600"> Can you please take look at the website that i made...</Text>
+            <Link href="https://ahita-soll-game-list.vercel.app/">Ahita-Soll-Game</Link>
           </Container>
         </Body>
       </Tailwind>
     </Html>
   );
 };
-
 // const body: CSSProperties = {
 //   background : "#fff"
 // }

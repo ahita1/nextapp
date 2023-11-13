aeb33a9c31878cb3cab51d44b09b8215bbb41dcd
ahita1 <ahitafani833@gmail.com>
2023-11-13 21:49:34 +0300

    Add new features

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import image1 from '@/public/images/Axin.png'
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+
+export default async function Home() {
+ const session = await getServerSession(authOptions)
+
+  return (
+    <>
+      <h1>Hey {session && <span>{ session.user!.name}</span> }</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard />
+      <main className='relative h-screen mt-5'>
+      <Image
+        src="https://bit.ly/react-cover" alt="reaact_images"
+        fill
+        className="object-cover"
+        sizes="(max-width : 480px) 100vw , (max-width : 768px) 50vw ,33vw "
+        quality={100}
+        priority
+      />
+     </main>
+    </>
+  )
+}

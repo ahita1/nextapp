e551881b823909c54c6ca77ed7507e32b85c8d33
ahita1 <ahitafani833@gmail.com>
2023-11-18 23:04:08 +0300

    Add new features

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className='btn btn-primary' onClick={() => console.log("button clicked haha!!!")}>
+        Add to Cart haha
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
